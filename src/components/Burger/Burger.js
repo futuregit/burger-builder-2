b05526eefe355af4b250d0ae4c84417eb31fb686
futuregit <gmail.com>
2019-05-21 17:11:04 -0400

    Create a list using array and map function for ingredients

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,11 +4,23 @@ import classes from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredients';
 
 const burger = (props) => {
+  //transformedIngredients takes an Object.keys which
+  //  collect the array keys from props.BurgerIngredients.
+  // The map takes the keys and and return a destructured
+  //  array. The destructured map method retrieve the indexes and
+  //  return BurgerIngredient component.
+
+  const transformedIngredients = Object.keys(props.ingredients)
+    .map(igKey =>{
+      return [...Array(props.ingredients[igKey])].map((_, i) => {
+        return <BurgerIngredient key={igKey + i} type={igKey} />;
+      });
+    });
+  console.log("Inside Burger transformedIngredients", transformedIngredients)
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
-      <BurgerIngredient type="salad" />
-      <BurgerIngredient type="meat" />
+      {transformedIngredients}
       <BurgerIngredient type="bread-bottom" />
     </div>
   );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,11 +3,21 @@ import Auxilliary from '../../hoc/Auxilliary';
 import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
-
+  // constructor(props){
+  //   super(props)
+  //   this.state = {...}
+  // }
+  state = {
+    ingredients: {
+      salad: 1,
+      bacon: 1,
+      meat: 2
+    }
+  }
   render(){
     return (
       <Auxilliary>
-        <Burger />
+        <Burger ingredients={this.state.ingredients}/>
         <div>Build Controls</div>
       </Auxilliary>
     );
