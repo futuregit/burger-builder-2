15478cf892fa8d7f607b86c2d4d41466f04c8770
futuregit <gmail.com>
2019-05-24 23:26:51 -0400

    Added sidebar

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 import Auxilliary from '../../hoc/Auxilliary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
 const layout = (props) => (
   <Auxilliary>
     <Toolbar />
+    <SideDrawer />
     <main className={classes.Content}>
       {props.children}
     </main>
