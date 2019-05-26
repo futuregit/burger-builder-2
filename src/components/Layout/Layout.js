3512d1c5cbef56d8e95e76cdf3648c80617d40e0
futuregit <gmail.com>
2019-05-26 00:51:35 -0400

    Added logic to remove backdrop with clicked

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,29 @@
-import React from 'react';
+import React, { Component} from 'react';
 import Auxilliary from '../../hoc/Auxilliary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
 
-const layout = (props) => (
-  <Auxilliary>
-    <Toolbar />
-    <SideDrawer />
-    <main className={classes.Content}>
-      {props.children}
-    </main>
-  </Auxilliary>
-);
+class Layout extends Component {
+  state = {
+    showSideDrawer: true
+  }
+  sideDrawerCloseHandler = () => {
+    this.setState({showSideDrawer: false});
+  }
+  render(){
+    return (
+    <Auxilliary>
+      <Toolbar />
+      <SideDrawer
+        open={this.state.showSideDrawer}
+        closed={this.sideDrawerCloseHandler}/>
+      <main className={classes.Content}>
+        {this.props.children}
+      </main>
+    </Auxilliary>
+  );
+  }
+}
 
-export default layout;
+export default Layout;
