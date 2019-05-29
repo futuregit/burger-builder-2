78b7e6e8ba3f9c1ec28a9d24110af9464a0e976f
futuregit <gmail.com>
2019-05-29 00:27:05 -0400

    Added shouldComponentUpdate for Modal component

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,20 +1,36 @@
-import React from 'react';
+import React, { Component } from 'react';
 import classes from './Modal.css';
 import Auxilliary from '../../../hoc/Auxilliary';
 import Backdrop from '../Backdrop/Backdrop';
 
-const modal = (props) => (
-  <Auxilliary>
-    <Backdrop show={props.show} clicked={props.modalClosed} />
-    <div
-      className={classes.Modal}
-      style={{
-        transform: props.show ? 'translateY(0)' : 'translateY(-100vh)',
-        opacity: props.show ?'1': '0'
-      }}>
-      {props.children}
-    </div>
-  </Auxilliary>
-)
+class Modal extends Component {
 
-export default modal;
+  shouldComponentUpdate(nextProps, nextState){
+    console.log("[Modal] shouldComponentUpdate")
+    return nextProps.show !== this.props.show;
+  }
+
+  componentWillUpdate(){
+    console.log("[Modal] componentWillUpdate")
+  }
+
+  render() {
+    return (
+      <Auxilliary>
+        <Backdrop show={this.props.show} clicked={this.props.modalClosed} />
+        <div
+          className={classes.Modal}
+          style={{
+            transform: this.props.show ? 'translateY(0)' : 'translateY(-100vh)',
+            opacity: this.props.show ?'1': '0'
+          }}>
+          {this.props.children}
+        </div>
+      </Auxilliary>
+    )
+  }
+};
+
+
+
+export default Modal;
