7d6c9005d859b3e5e92098214242e003b13bca36
futuregit <gmail.com>
2019-06-25 12:08:23 -0400

    Added checkout container

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React, { Component } from 'react';
 import Layout from './hoc/Layout/Layout';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
+import Checkout from './containers/Checkout/Checkout';
 
 class App extends Component {
   state = {
     show: true
   }
 
-  componentDidMount() {
-    setTimeout(() => {
-      this.setState({show: false});
-    }, 5000);
-  }
+  // componentDidMount() {
+  //   setTimeout(() => {
+  //     this.setState({show: false});
+  //   }, 5000);
+  // }
   render() {
     return (
       <div>
         <Layout>
           {this.state.show ? <BurgerBuilder /> : null}
+          <Checkout />
         </Layout>
       </div>
     );
