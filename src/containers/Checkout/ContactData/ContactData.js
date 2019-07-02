279d7afd563fb387f642f1167b2ad1e407e56b32
futuregit <gmail.com>
2019-07-02 16:06:12 -0400

    Added visual background change for form validation

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -19,7 +19,8 @@ class ContactData extends Component {
           validation: {
             required: true
           },
-          valid: false
+          valid: false,
+          touched: false
         },
         street: {
           elementType: 'input',
@@ -31,7 +32,8 @@ class ContactData extends Component {
           validation: {
             required: true
           },
-          valid: false
+          valid: false,
+          touched: false
         },
         zipcode:  {
           elementType: 'input',
@@ -45,7 +47,8 @@ class ContactData extends Component {
             minLength: 5,
             maxLength: 5
           },
-          valid: false
+          valid: false,
+          touched: false
         },
         country:  {
           elementType: 'input',
@@ -57,7 +60,8 @@ class ContactData extends Component {
           validation: {
             required: true
           },
-          valid: false
+          valid: false,
+          touched: false
         },
         email:  {
           elementType: 'input',
@@ -69,7 +73,8 @@ class ContactData extends Component {
           validation: {
             required: true
           },
-          valid: false
+          valid: false,
+          touched: false
         },
         deliveryMethod:  {
           elementType: 'select',
@@ -133,6 +138,7 @@ class ContactData extends Component {
     console.log("Inside ContactData inputChangedHandler updatedFormElement value", updatedFormElement)
     updatedFormElement.value = event.target.value;
     updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+    updatedFormElement.touched = true;
     updatedOrderForm[inputIdentifier] = updatedFormElement;
     console.log("Inside ContactData inputChangedHandler updatedFormElement value", updatedFormElement);
     console.log("Inside ContactData inputChangedHandler updatedOrderForm value", updatedOrderForm)
@@ -158,6 +164,9 @@ class ContactData extends Component {
             elementType={formElement.config.elementType}
             elementConfig={formElement.config.elementConfig}
             value={formElement.config.value}
+            invalid={!formElement.config.valid}
+            shouldValidate={formElement.config.validation}
+            touched={formElement.config.touched}
             changed={(event) => this.inputChangedHandler(event, formElement.id)} />
         ))}
         <Button btnType="Success">ORDER</Button>
