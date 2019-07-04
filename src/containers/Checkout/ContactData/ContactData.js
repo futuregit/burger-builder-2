b4eb5764e9e81f931fe2ee575136c5852e01d9c6
futuregit <gmail.com>
2019-07-04 13:21:23 -0400

    Added more form validity and styling of button

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -83,9 +83,11 @@ class ContactData extends Component {
                       {value: 'Cheapest', displayValue: 'Cheapest'}
                     ]
           },
-          value: ''
+          value: '',
+          valid: true
         }
     },
+    fromIsValid: false,
     loading: false
   }
 
@@ -143,8 +145,11 @@ class ContactData extends Component {
     console.log("Inside ContactData inputChangedHandler updatedFormElement value", updatedFormElement);
     console.log("Inside ContactData inputChangedHandler updatedOrderForm value", updatedOrderForm)
     console.log("Inside ContactData inputChangedHandler state.orderForm value", this.state.orderForm)
-
-    this.setState({orderForm: updatedOrderForm});
+    let formIsValid = true;
+    for (let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid;
+    }
+    this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
   }
 
   render() {
@@ -169,7 +174,7 @@ class ContactData extends Component {
             touched={formElement.config.touched}
             changed={(event) => this.inputChangedHandler(event, formElement.id)} />
         ))}
-        <Button btnType="Success">ORDER</Button>
+        <Button btnType="Success" disabled={!this.state.formIsValid}>ORDER</Button>
       </form>
     );
     if (this.state.loading){
