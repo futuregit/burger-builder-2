3d59751aa6e255bae939fca5f587c9da4b54ba31
futuregit <gmail.com>
2019-07-25 14:25:36 -0400

    Refactored reducers using function to copy state

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,4 +1,5 @@
 import * as actionTypes from '../actions/actionTypes';
+import { updateObject } from '../utility';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -15,40 +16,29 @@ const initialState = {
 
 const reducer = (state = initialState, action) => {
   switch (action.type){
-    case 'LOAD_INGREDIENTS':
+    case actionTypes.LOAD_INGREDIENTS:
       return {
         ...state,
         ingredients: action.loadIgns
-      }
+      };
     case actionTypes.ADD_INGREDIENT:
-      return {
-          // spread out the top level object and copy to a new object.
-        ...state,
-        ingredients: {
-          // spread out the nested objects and copy to a new object.
-          ...state.ingredients,
-          //ES6 to get the property name. If ingredientName is bacon, this
-          // will translates to state.ingredients.bacon.
-          [action.ingredientName]: state.ingredients[action.ingredientName] + 1
-        },
+      const updatedIngredient = {  [action.ingredientName]: state.ingredients[action.ingredientName] + 1 };
+      const updatedIngredients = updateObject(state.ingredients, updatedIngredient);
+      const updatedState = {
+        ingredients: updatedIngredients,
         totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName]
-      };
+      }
+      return updateObject(state, updatedState);
     case actionTypes.REMOVE_INGREDIENT:
-      return {
-        // spread out the top level object and copy to a new object.
-      ...state,
-      ingredients: {
-        // spread out the nested objects and copy to a new object.
-        ...state.ingredients,
-        //ES6 to get the property name. If ingredientName is bacon, this
-        // will translates to state.ingredients.bacon.
-        [action.ingredientName]: state.ingredients[action.ingredientName] - 1
-      },
-      totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
-      };
+      const updatedIng = {  [action.ingredientName]: state.ingredients[action.ingredientName] - 1 };
+      const updatedIngs = updateObject(state.ingredients, updatedIng);
+      const updatedSt = {
+        ingredients: updatedIngs,
+        totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName]
+      }
+      return updateObject(state, updatedSt);
       case actionTypes.SET_INGREDIENTS:
-        return {
-          ...state,
+        return updateObject(state, {
           ingredients: {
             salad: action.ingredients.salad,
             bacon: action.ingredients.bacon,
@@ -57,12 +47,9 @@ const reducer = (state = initialState, action) => {
           },
           totalPrice: 4,
           error: false
-        };
+        });
       case actionTypes.FETCH_INGREDIENTS_FAILED:
-        return {
-          ...state,
-          error: true
-        };
+        return updateObject(state, {error: true});
     default:
       return state;
   }
