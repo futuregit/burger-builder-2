cc9eef97e382ac3f79c177ab64858c923b4153d8
futuregit <gmail.com>
2019-07-28 03:17:25 -0400

    Added sign in

--- src/store/actions/auth.js
+++ src/store/actions/auth.js
@@ -21,15 +21,20 @@ export const authFail = (error) => {
   };
 };
 
-export const auth = (email, password) => {
+export const auth = (email, password, isSignup) => {
   return dispatch => {
     dispatch(authStart());
     const authData = {
       email: email,
       password: password,
       returnSecureToken: true
+    };
+    let token = '';
+    let url = 'https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=' + token;
+    if (!isSignup) {
+      url = 'https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=' + token;
     }
-    axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=', authData)
+    axios.post(url, authData)
       .then(response => {
         console.log(response);
         dispatch(authSuccess(response.data));
